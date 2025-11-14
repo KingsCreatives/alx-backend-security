28c2d8e6ca8fd3a3ea3df53e46e194ffe6fd8802
kwame cody <kwamecody@gmail.com>
2025-11-14 06:35:43 +0000

    populate city and country and cache results for 24 hours

--- ip_tracking/middleware.py
+++ ip_tracking/middleware.py
@@ -1,5 +1,7 @@
 from ipware import get_client_ip
 from django.http import HttpResponseForbidden
+from django.core.cache import cache
+from django_ip_geolocation.utils import get_geolocation
 from .models import RequestLog, BlockedIP
 
 class IPLoggingMiddleware:
@@ -12,9 +14,22 @@ class IPLoggingMiddleware:
         if ip is not None:
             if ip in self.blocked_ips:
                 return HttpResponseForbidden("Access Denied: Your IP address is blacklisted")
+            
+            geo_data = cache.get(ip)
+
+            if geo_data is None:
+                geo_data = get_geolocation(ip)
+                cache.set(ip, geo_data, timeout=86400)
+            
+            country = geo_data.get('country_name')
+            city = geo_data.get('city')
+
             RequestLog.objects.create(
                 ip_address=ip,
                 path=request.path,
+                country=country,
+                city=city,
             )
+
         response = self.get_response(request)
         return response
--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -9,9 +9,11 @@ class RequestLog(models.Model):
     )
     path = models.CharField(max_length=255)
     timestamp = models.DateTimeField(auto_now_add=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    city= models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.ip_address} accessed {self.path} at {self.timestamp.strftime('%Y-%m-%d %H:%M:%S')}"
+        return f"{self.ip_address} ({self.country}) accessed {self.path}"
 
 
 class BlockedIP(models.Model):
